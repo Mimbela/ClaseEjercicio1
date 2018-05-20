6bb578eb7223b4365e9fd67e6a93f28cdde38c18
senin8 <malizziaaries@gmail.com>
2018-05-20 14:21:34 +0200

    1

--- ClaseEjercicio1/Program.cs
+++ ClaseEjercicio1/Program.cs
@@ -10,6 +10,28 @@ namespace ClaseEjercicio1
     {
         static void Main(string[] args)
         {
+            //características del objeto puerta
+            Puerta puerta = new Puerta();
+            puerta.Alto = 150;
+            puerta.Ancho = 100;
+            puerta.Color = 23;
+            puerta.Abrir();
+            puerta.MostrarEstado();
+
+            Console.WriteLine("");
+
+            Puerta puerta2 = new Puerta();
+            puerta2.Alto = 90;
+            puerta2.Ancho = 50;
+            puerta2.Color = 45;
+            puerta2.Abrir();
+            puerta2.MostrarEstado();
+
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ClaseEjercicio1/Puerta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseEjercicio1
+{
+    public class Puerta
+    {
+        public int Ancho { get; set; }//anchura de la puerta
+        public int Alto { get; set; }//alto de la puerta
+        public int Color { get; set; }// color de la puerta
+
+        public bool Abierta { get; set; }//abierta o cerrada
+
+        public  void Abrir ()
+        {
+            Abierta = true;
+        }
+
+        public void Cerrar()
+        {
+            Abierta = false;
+        }
+
+        public void MostrarEstado()
+        {
+            Console.WriteLine("Ancho {0}",Ancho);
+            Console.WriteLine("Alto {0}", Alto);
+            Console.WriteLine("Color{0}",Color);
+            Console.WriteLine("Abierta {0}",Abierta);
+
+
+        }
+    }
+}
